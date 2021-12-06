42d89a08e56bbfbd1ec20e0aeb09d41f2ee374e7
hua ming <hmbig2@163.com>
2021-12-06 16:47:53 +0800

    feat(rds): add some parameter in flavors api (#71)

--- openstack/rds/v3/flavors/results.go
+++ openstack/rds/v3/flavors/results.go
@@ -8,11 +8,14 @@ type DbFlavorsResp struct {
 	Flavorslist []Flavors `json:"flavors"`
 }
 type Flavors struct {
+	ID           string            `json:"id" `
 	Vcpus        string            `json:"vcpus" `
 	Ram          int               `json:"ram" `
 	Speccode     string            `json:"spec_code"  `
 	Instancemode string            `json:"instance_mode" `
 	Azstatus     map[string]string `json:"az_status" `
+	VersionName  []string          `json:"version_name" `
+	GroupType    string            `json:"group_type" `
 }
 
 type DbFlavorsPage struct {
