715faeedf5358f7eb28cb74b2d688fdeae211bb6
Lance52259 <Overlord52259@gmail.com>
2022-03-03 16:42:31 +0800

    feat(fgs/dependency): add new dependent package methods

--- openstack/fgs/v2/dependencies/testing/requests_test.go
+++ openstack/fgs/v2/dependencies/testing/requests_test.go
@@ -9,6 +9,26 @@ import (
 	"github.com/chnsz/golangsdk/testhelper/client"
 )
 
+func TestCreateV2Dependency(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV2DependencyCreate(t)
+
+	actual, err := dependencies.Create(client.ServiceClient(), createOpts)
+	th.AssertNoErr(t, err)
+	th.AssertDeepEquals(t, &expectedGetResponseData, actual)
+}
+
+func TestGetV2Dependency(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV2DependencyGet(t)
+
+	actual, err := dependencies.Get(client.ServiceClient(), "e6cc2ebe-0bae-4b69-a1d6-8198bc356ff8")
+	th.AssertNoErr(t, err)
+	th.AssertDeepEquals(t, &expectedGetResponseData, actual)
+}
+
 func TestListV2Dependencies(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
@@ -24,4 +44,23 @@ func TestListV2Dependencies(t *testing.T) {
 		})
 	th.AssertNoErr(t, err)
 	th.CheckDeepEquals(t, expectedListResponseData, actual)
+}
+
+func TestUpdateV2Dependency(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV2DependencyUpdate(t)
+
+	actual, err := dependencies.Update(client.ServiceClient(), "e6cc2ebe-0bae-4b69-a1d6-8198bc356ff8", createOpts)
+	th.AssertNoErr(t, err)
+	th.AssertDeepEquals(t, &expectedGetResponseData, actual)
+}
+
+func TestDeleteV2Dependency(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV2DependencyDelete(t)
+
+	err := dependencies.Delete(client.ServiceClient(), "e6cc2ebe-0bae-4b69-a1d6-8198bc356ff8").ExtractErr()
+	th.AssertNoErr(t, err)
 }
