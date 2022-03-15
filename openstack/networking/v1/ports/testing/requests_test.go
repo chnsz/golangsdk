8b5a0e685be38273554d5993df842c95f6021613
Lance <Overlord52259@gmail.com>
2022-03-15 10:43:38 +0800

    feat(networking/vip): add new sdk support (#133)

---
+++ openstack/networking/v1/ports/testing/requests_test.go
@@ -0,0 +1,48 @@
+package testing
+
+import (
+	"testing"
+
+	"github.com/chnsz/golangsdk/openstack/networking/v1/ports"
+	th "github.com/chnsz/golangsdk/testhelper"
+	"github.com/chnsz/golangsdk/testhelper/client"
+)
+
+func TestCreateV1NetworkVIP(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV1NetworkVIPCreate(t)
+
+	actual, err := ports.Create(client.ServiceClient(), createOpts)
+	th.AssertNoErr(t, err)
+	th.AssertDeepEquals(t, expectedCreateResponseData, actual)
+}
+
+func TestGetV1NetworkVIP(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV1NetworkVIPGet(t)
+
+	actual, err := ports.Get(client.ServiceClient(), "05547c10-e318-4067-9db2-01f5dc30be38")
+	th.AssertNoErr(t, err)
+	th.AssertDeepEquals(t, expectedCreateResponseData, actual)
+}
+
+func TestUpdateV1NetworkVIP(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV1NetworkVIPUpdate(t)
+
+	actual, err := ports.Update(client.ServiceClient(), "05547c10-e318-4067-9db2-01f5dc30be38", updateOpts)
+	th.AssertNoErr(t, err)
+	th.AssertDeepEquals(t, expectedCreateResponseData, actual)
+}
+
+func TestDeleteV1NetworkVIP(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV1NetworkVIPDelete(t)
+
+	err := ports.Delete(client.ServiceClient(), "05547c10-e318-4067-9db2-01f5dc30be38").ExtractErr()
+	th.AssertNoErr(t, err)
+}
