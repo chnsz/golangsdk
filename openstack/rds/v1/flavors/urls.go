f0f9781bf5548d384b9228480051c9449371abcf
hua ming <hmbig2@163.com>
2022-03-25 10:15:13 +0800

    chore: remove the some func which buildURL in urls (#136)

--- openstack/rds/v1/flavors/urls.go
+++ openstack/rds/v1/flavors/urls.go
@@ -2,7 +2,6 @@ package flavors
 
 import "github.com/chnsz/golangsdk"
 
-func listURL(c *golangsdk.ServiceClient, dataStoreID string, region string) string {
-
-	return c.ResourceBaseURL() + "flavors?dbId=" + dataStoreID + "&region=" + region
+func listURL(c *golangsdk.ServiceClient) string {
+	return c.ServiceURL("flavors")
 }
