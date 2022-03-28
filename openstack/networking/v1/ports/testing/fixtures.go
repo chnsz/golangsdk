3a61888f49c5e64e67d37622d8e20dcd8f04c748
ShiChangkuo <shichangkuo90@gmail.com>
2022-03-28 11:18:56 +0800

    feat(ports): add List support and testing case

--- openstack/networking/v1/ports/testing/fixtures.go
+++ openstack/networking/v1/ports/testing/fixtures.go
@@ -10,10 +10,37 @@ import (
 	th "github.com/chnsz/golangsdk/testhelper"
 )
 
-const (
-	expectedCreateResponse = `
+var (
+	expectedCreateResponse = fmt.Sprintf(`
 {
-	"port": {
+	"port": %s
+}`, buildPortItemResponse("192.168.0.162"))
+
+	expectedUpdateResponse = fmt.Sprintf(`
+{
+	"port": %s
+}`, buildPortItemResponse("192.168.0.25"))
+
+	expectedListResponse = fmt.Sprintf(`
+{
+	"ports": [
+		%s
+	]
+}`, buildPortItemResponse("192.168.0.162"))
+
+	emptyListResponse = `
+{
+	"ports": []
+}`
+
+	expectedCreateResponseData = buildPortResponseData("192.168.0.162")
+	expectedUpdateResponseData = buildPortResponseData("192.168.0.25")
+	expectedListResponseData   = []ports.Port{buildPortResponseData("192.168.0.162")}
+)
+
+func buildPortItemResponse(address string) string {
+	return fmt.Sprintf(`
+	{
 		"admin_state_up": false,
 		"allowed_address_pairs": [
 		  {
@@ -26,7 +53,7 @@ const (
 		"device_owner": "neutron:VIP_PORT",
 		"fixed_ips": [
 			{
-				"ip_address": "192.168.0.162",
+				"ip_address": "%s",
 				"subnet_id": "885cb8c3-0cbe-406d-83d6-fc98856fcf26"
 			}
 		],
@@ -34,23 +61,11 @@ const (
 		"mac_address": "fa:16:3e:71:db:e5",
 		"network_id": "e4cb3b49-78a0-479b-b37d-bd99b3ec0d8a",
 		"status": "DOWN"
-	}
-}`
-)
+	}`, address)
+}
 
-var (
-	createOpts = ports.CreateOpts{
-		DeviceOwner: "neutron:VIP_PORT",
-		FixedIps: []ports.FixedIp{
-			{
-				IpAddress: "192.168.0.162",
-				SubnetId:  "885cb8c3-0cbe-406d-83d6-fc98856fcf26",
-			},
-		},
-		NetworkId: "e4cb3b49-78a0-479b-b37d-bd99b3ec0d8a",
-	}
-
-	expectedCreateResponseData = &ports.Port{
+func buildPortResponseData(address string) ports.Port {
+	return ports.Port{
 		AdminStateUp: false,
 		AllowedAddressPairs: []ports.AddressPair{
 			{
@@ -63,7 +78,7 @@ var (
 		DeviceOwner: "neutron:VIP_PORT",
 		FixedIps: []ports.FixedIp{
 			{
-				IpAddress: "192.168.0.162",
+				IpAddress: address,
 				SubnetId:  "885cb8c3-0cbe-406d-83d6-fc98856fcf26",
 			},
 		},
@@ -72,17 +87,9 @@ var (
 		MacAddress: "fa:16:3e:71:db:e5",
 		Status:     "DOWN",
 	}
+}
 
-	updateOpts = ports.UpdateOpts{
-		AllowedAddressPairs: []ports.AddressPair{
-			{
-				IpAddress: "192.168.0.25",
-			},
-		},
-	}
-)
-
-func handleV1NetworkVIPCreate(t *testing.T) {
+func handleV1NetworkPortCreate(t *testing.T) {
 	th.Mux.HandleFunc("/v1/85636478b0bd8e67e89469c7749d4127/ports", func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "POST")
 		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
@@ -92,7 +99,7 @@ func handleV1NetworkVIPCreate(t *testing.T) {
 	})
 }
 
-func handleV1NetworkVIPGet(t *testing.T) {
+func handleV1NetworkPortGet(t *testing.T) {
 	th.Mux.HandleFunc("/v1/85636478b0bd8e67e89469c7749d4127/ports/05547c10-e318-4067-9db2-01f5dc30be38", func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "GET")
 		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
@@ -102,21 +109,38 @@ func handleV1NetworkVIPGet(t *testing.T) {
 	})
 }
 
-func handleV1NetworkVIPUpdate(t *testing.T) {
+func handleV1NetworkPortUpdate(t *testing.T) {
 	th.Mux.HandleFunc("/v1/85636478b0bd8e67e89469c7749d4127/ports/05547c10-e318-4067-9db2-01f5dc30be38", func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "PUT")
 		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, expectedCreateResponse)
+		fmt.Fprint(w, expectedUpdateResponse)
 	})
 }
 
-func handleV1NetworkVIPDelete(t *testing.T) {
+func handleV1NetworkPortDelete(t *testing.T) {
 	th.Mux.HandleFunc("/v1/85636478b0bd8e67e89469c7749d4127/ports/05547c10-e318-4067-9db2-01f5dc30be38", func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "DELETE")
 		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusNoContent)
+	})
+}
+
+func handleV1NetworkPortList(t *testing.T) {
+	th.Mux.HandleFunc("/v1/85636478b0bd8e67e89469c7749d4127/ports", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		r.ParseForm()
+		marker := r.Form.Get("marker")
+		if marker == "" {
+			fmt.Fprint(w, expectedListResponse)
+		} else {
+			fmt.Fprint(w, emptyListResponse)
+		}
 	})
 }
