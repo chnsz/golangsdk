6b7cdcd8a5062dcade1aa5c2d6a0440a3cdb2765
shichangkuo <shichangkuo90@gmail.com>
2022-03-28 11:28:52 +0800

    feat(ports): add List support and testing case (#142)

--- openstack/networking/v1/ports/testing/requests_test.go
+++ openstack/networking/v1/ports/testing/requests_test.go
@@ -8,41 +8,76 @@ import (
 	th "github.com/chnsz/golangsdk/testhelper"
 )
 
-func TestCreateV1NetworkVIP(t *testing.T) {
+func TestCreateV1NetworkPort(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
-	handleV1NetworkVIPCreate(t)
+	handleV1NetworkPortCreate(t)
+
+	createOpts := ports.CreateOpts{
+		DeviceOwner: "neutron:VIP_PORT",
+		FixedIps: []ports.FixedIp{
+			{
+				IpAddress: "192.168.0.162",
+				SubnetId:  "885cb8c3-0cbe-406d-83d6-fc98856fcf26",
+			},
+		},
+		NetworkId: "e4cb3b49-78a0-479b-b37d-bd99b3ec0d8a",
+	}
 
 	actual, err := ports.Create(client.ServiceClient(), createOpts)
 	th.AssertNoErr(t, err)
-	th.AssertDeepEquals(t, expectedCreateResponseData, actual)
+	th.AssertDeepEquals(t, &expectedCreateResponseData, actual)
 }
 
-func TestGetV1NetworkVIP(t *testing.T) {
+func TestGetV1NetworkPort(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
-	handleV1NetworkVIPGet(t)
+	handleV1NetworkPortGet(t)
 
 	actual, err := ports.Get(client.ServiceClient(), "05547c10-e318-4067-9db2-01f5dc30be38")
 	th.AssertNoErr(t, err)
-	th.AssertDeepEquals(t, expectedCreateResponseData, actual)
+	th.AssertDeepEquals(t, &expectedCreateResponseData, actual)
 }
 
-func TestUpdateV1NetworkVIP(t *testing.T) {
+func TestUpdateV1NetworkPort(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
-	handleV1NetworkVIPUpdate(t)
+	handleV1NetworkPortUpdate(t)
+
+	updateOpts := ports.UpdateOpts{
+		AllowedAddressPairs: []ports.AddressPair{
+			{
+				IpAddress: "192.168.0.25",
+			},
+		},
+	}
 
 	actual, err := ports.Update(client.ServiceClient(), "05547c10-e318-4067-9db2-01f5dc30be38", updateOpts)
 	th.AssertNoErr(t, err)
-	th.AssertDeepEquals(t, expectedCreateResponseData, actual)
+	th.AssertDeepEquals(t, &expectedUpdateResponseData, actual)
 }
 
-func TestDeleteV1NetworkVIP(t *testing.T) {
+func TestDeleteV1NetworkPort(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
-	handleV1NetworkVIPDelete(t)
+	handleV1NetworkPortDelete(t)
 
 	err := ports.Delete(client.ServiceClient(), "05547c10-e318-4067-9db2-01f5dc30be38").ExtractErr()
 	th.AssertNoErr(t, err)
+}
+
+func TestListV1NetworkPort(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleV1NetworkPortList(t)
+
+	listOpts := ports.ListOpts{
+		ID: "05547c10-e318-4067-9db2-01f5dc30be38",
+	}
+	allPages, err := ports.List(client.ServiceClient(), listOpts).AllPages()
+	th.AssertNoErr(t, err)
+
+	actual, err := ports.ExtractPorts(allPages)
+	th.AssertNoErr(t, err)
+	th.AssertDeepEquals(t, expectedListResponseData, actual)
 }
