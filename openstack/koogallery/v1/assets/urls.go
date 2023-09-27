d3145011730bdfd4a46447a85523b6e54b96750e
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2023-09-27 10:25:19 +0800

    fix(koogallery): use new url (#428)

--- openstack/koogallery/v1/assets/urls.go
+++ openstack/koogallery/v1/assets/urls.go
@@ -3,5 +3,5 @@ package assets
 import "github.com/chnsz/golangsdk"
 
 func listURL(sc *golangsdk.ServiceClient) string {
-	return sc.ServiceURL("assset", "deployed-object")
+	return sc.ServiceURL("asset", "deployed-object")
 }
