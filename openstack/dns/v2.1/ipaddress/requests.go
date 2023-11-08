379c9360c1d4478796545c310f56030c72a4e9a7
dangyy0626 <dangyy0626@163.com>
2023-11-08 16:16:30 +0800

    feat(dns): Add dns ipaddress sdk

---
+++ openstack/dns/v2.1/ipaddress/requests.go
@@ -0,0 +1,23 @@
+package ipaddress
+
+import "github.com/chnsz/golangsdk"
+
+func Create(c *golangsdk.ServiceClient, opts CreateOpts, endpointID string) (r CreateResult) {
+	body, err := golangsdk.BuildRequestBody(opts, "")
+	if err != nil {
+		r.Err = err
+		return
+	}
+	_, r.Err = c.Post(baseUrl(c, endpointID), body, &r.Body, nil)
+	return
+}
+
+func Delete(c *golangsdk.ServiceClient, endpointID string, ipaddressID string) (r DeleteResult) {
+	_, r.Err = c.Delete(resourceUrl(c, endpointID, ipaddressID), nil)
+	return
+}
+
+func List(c *golangsdk.ServiceClient, endpointID string) (r ListResult) {
+	_, r.Err = c.Get(baseUrl(c, endpointID), &r.Body, nil)
+	return
+}
