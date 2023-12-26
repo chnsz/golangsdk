2d01504ab0eaed76ea39ea0541156d00ac13169b
MrLiangSir <17792610566@163.com>
2023-12-26 19:48:09 +0800

    feat(Anti-DDoS): antiddos resource support alarm configuration

--- openstack/antiddos/v2/alarmreminding/results.go
+++ openstack/antiddos/v2/alarmreminding/results.go
@@ -24,24 +24,31 @@ type WarnAlertResponse struct {
 		// DDoS attacks
 		AntiDDoS bool `json:"antiDDoS,"`
 
+		// Deprecated
 		// Brute force cracking (system logins, FTP, and DB)
 		BruceForce bool `json:"bruce_force,omitempty"`
 
+		// Deprecated
 		// Alarms about remote logins
 		RemoteLogin bool `json:"remote_login,omitempty"`
 
+		// Deprecated
 		// Weak passwords (system and database)
 		WeakPassword bool `json:"weak_password,omitempty"`
 
+		// Deprecated
 		// Overly high rights of a database process
 		HighPrivilege bool `json:"high_privilege,omitempty"`
 
+		// Deprecated
 		// Web page backdoors
 		BackDoors bool `json:"back_doors,omitempty"`
 
+		// Deprecated
 		// Reserved
 		Waf bool `json:"waf,omitempty"`
 
+		// Deprecated
 		// Possible values: 0: indicates that alarms are sent once a day. 1: indicates that alarms are sent once every half hour. This parameter is mandatory for the Host Intrusion Detection (HID) service.
 		SendFrequency int `json:"send_frequency,omitempty"`
 	} `json:"warn_config,"`
@@ -51,4 +58,14 @@ type WarnAlertResponse struct {
 
 	// Description of an alarm group
 	DisplayName string `json:"display_name,"`
+}
+
+type UpdateResult struct {
+	commonResult
+}
+
+func (r UpdateResult) Extract() (*WarnAlertResponse, error) {
+	var response WarnAlertResponse
+	err := r.ExtractInto(&response)
+	return &response, err
 }
--- openstack/antiddos/v2/alarmreminding/urls.go
+++ openstack/antiddos/v2/alarmreminding/urls.go
@@ -4,6 +4,13 @@ import (
 	"github.com/chnsz/golangsdk"
 )
 
-func WarnAlertURL(c *golangsdk.ServiceClient) string {
-	return c.ServiceURL("warnalert", "alertconfig", "query")
+const resourcePathWarnAlert = "warnalert"
+const resourcePathAlertConfig = "alertconfig"
+
+func GetWarnAlertURL(c *golangsdk.ServiceClient) string {
+	return c.ServiceURL(resourcePathWarnAlert, resourcePathAlertConfig, "query")
+}
+
+func UpdateWarnAlertURL(c *golangsdk.ServiceClient) string {
+	return c.ServiceURL(resourcePathWarnAlert, resourcePathAlertConfig, "update")
 }
