866add511311ae48e64036decf8eac8c49ffc300
zhangting951007 <1719626126@qq.com>
2024-01-08 09:43:35 +0800

    fix(EVS): update the request method and  request body of the modifying QoS API (#518)

--- openstack/evs/v5/cloudvolumes/requests.go
+++ openstack/evs/v5/cloudvolumes/requests.go
@@ -4,12 +4,16 @@ import (
 	"github.com/chnsz/golangsdk"
 )
 
-type QoSUpdateOpts struct {
+type QoSModifyOpts struct {
+	IopsAndThroughputOpts IopsAndThroughputOpts `json:"qos_modify" required:"true"`
+}
+
+type IopsAndThroughputOpts struct {
 	Iops       int `json:"iops" required:"true"`
 	Throughput int `json:"throughput,omitempty"`
 }
 
-func (opts QoSUpdateOpts) ToVolumeUpdateQoSMap() (map[string]interface{}, error) {
+func (opts QoSModifyOpts) ToVolumeUpdateQoSMap() (map[string]interface{}, error) {
 	return golangsdk.BuildRequestBody(opts, "")
 }
 
@@ -17,13 +21,12 @@ type UpdateQoSOptsBuilder interface {
 	ToVolumeUpdateQoSMap() (map[string]interface{}, error)
 }
 
-func UpdateQoS(client *golangsdk.ServiceClient, id string, opts UpdateQoSOptsBuilder) (r JobResult) {
+func ModifyQoS(client *golangsdk.ServiceClient, id string, opts UpdateQoSOptsBuilder) (r JobResult) {
 	b, err := opts.ToVolumeUpdateQoSMap()
 	if err != nil {
 		r.Err = err
 		return
 	}
-
-	_, r.Err = client.Post(qoSURL(client, id), b, &r.Body, nil)
+	_, r.Err = client.Put(qoSURL(client, id), b, &r.Body, nil)
 	return
 }
