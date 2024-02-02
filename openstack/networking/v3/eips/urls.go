4dda280234102d155bf834cd66559a6f4128e931
saf3dfsa <caiwanting@huawei.com>
2024-02-02 15:37:53 +0800

    chore(eip): change v3 eip url

--- openstack/networking/v3/eips/urls.go
+++ openstack/networking/v3/eips/urls.go
@@ -5,5 +5,5 @@ import "github.com/chnsz/golangsdk"
 const resourcePath = "publicips"
 
 func getURL(client *golangsdk.ServiceClient, id string) string {
-	return client.ServiceURL(client.ProjectID, "eip", resourcePath, id)
+	return client.ServiceURL("eip", resourcePath, id)
 }
